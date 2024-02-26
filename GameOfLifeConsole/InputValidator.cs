e65ccec938dca4eb5523f6d63fd519d4f3dea1e5
jburdin7 <j.burdin7@gmail.com>
2024-02-25 21:01:57 -0500

    Added menu options.

---
+++ GameOfLifeConsole/InputValidator.cs
@@ -0,0 +1,29 @@
+﻿namespace GameOfLifeConsole
+{
+    class InputValidator
+    {
+        private string[] ValidChoices { get; set; }
+
+        public InputValidator(string[] validChoices)
+        {
+            this.ValidChoices = validChoices;
+        }
+
+        public bool ValidateString(string input)
+        {
+            if (!string.IsNullOrWhiteSpace(input))
+            {
+                if(IsValidString(input, ValidChoices))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool IsValidString(string input, string[] validChoices)
+        {
+            return validChoices.Contains(input.ToLower());
+        }
+    }
+}
--- GameOfLifeConsole/Program.cs
+++ GameOfLifeConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using GameOfLifeConsole;
 using System;
+using System.Runtime.CompilerServices;
 using System.Threading;
 
 class GameOfLife
@@ -7,16 +8,58 @@ class GameOfLife
 
     public static void Main()
     {
-        Grid grid = new Grid(20);
-        //TODO: Users choose dimesions of grid
-        //TODO: Users choose the speed of the simulation
+        
+        InputValidator stringInputValidator = new InputValidator(
+            new[] {
+                "start",
+                "help",
+                "close"
+            });
+        bool inMenu = true;
 
-        while (true)
+        while (inMenu)
         {
-            Console.Clear();
-            grid.DrawGrid();
-            grid.UpdateGrid();
-            Thread.Sleep(100);
-        }
+            Console.WriteLine("~Conway's Game of Life~");
+            Console.WriteLine("|----Menu-----|");
+            Console.WriteLine("| Start       |\r\n| About Game        |\r\n| Close       |");
+            Console.WriteLine("|-------------|\r\n");
+
+            string userInput = Console.ReadLine();
+            bool isValidUserChoice = stringInputValidator.ValidateString(userInput);
+            bool isRunning = false;
+
+            if (isValidUserChoice)
+            {
+                if (userInput.Equals("start"))
+                {
+                    isRunning = true;
+                }
+                else if (userInput.Equals("about game"))
+                {
+                    Console.WriteLine("Explains how Conway's game of life work."); //please change this to be helpful
+                }
+                else if (userInput.Equals("close"))
+                {
+                    Environment.Exit(0); //NOTES: or set 'inMenu' to 'false'
+                }
+            }
+            else
+            {
+                Console.WriteLine("Incorrect Choice: Please select from the menu options.\r\n");
+                Thread.Sleep(1000);
+            }
+
+            Grid grid = new Grid(20);
+            //TODO: Users choose dimesions of grid
+            //TODO: Users choose the speed of the simulation
+
+            while (isRunning)
+            {
+                Console.Clear();
+                grid.DrawGrid();
+                grid.UpdateGrid();
+                Thread.Sleep(100);
+            }
+        } 
     }
 }
